8ae436ababd0a9bd3338e8e0244732bccf55c9f1
FHuntet05 <129078356+FHuntet05@users.noreply.github.com>
2023-09-27 01:10:37 -0400

    config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/5fc119c05326d2595965f.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1601545124').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001857528168") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001857528168))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
