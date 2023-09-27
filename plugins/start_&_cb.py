64811a7f1297639efb2e63dfc637e0ff18e04014
FHuntet05 <129078356+FHuntet05@users.noreply.github.com>
2023-09-27 11:17:51 -0400

    start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -11,13 +11,13 @@ async def start(client, message):
     user = message.from_user
     await db.add_user(client, message)                
     button = InlineKeyboardMarkup([[
-        InlineKeyboardButton("👨‍💻 Dev 👨‍💻", callback_data='dev')
+        InlineKeyboardButton("👨‍💻 Dev 💻", callback_data='dev')
         ],[
-        InlineKeyboardButton('📯 Updates', url='https://t.me/fsremote'),
-        InlineKeyboardButton('💁‍♂️ Soporte', url='https://t.me/feft05')
+        InlineKeyboardButton('♻️ Updates', url='https://t.me/fsremote'),
+        InlineKeyboardButton('🗨️ Soporte', url='https://t.me/feft05')
         ],[
-        InlineKeyboardButton('🎛️ About', callback_data='about'),
-        InlineKeyboardButton('🛠️ Help', callback_data='help')
+        InlineKeyboardButton('📋 About', callback_data='about'),
+        InlineKeyboardButton('👮‍♂️ Help', callback_data='help')
     ]])
     if Config.START_PIC:
         await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
@@ -33,13 +33,13 @@ async def cb_handler(client, query: CallbackQuery):
             text=Txt.START_TXT.format(query.from_user.mention),
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup([[
-                InlineKeyboardButton("👨‍💻 Dev 👨‍💻", callback_data='dev')
+                InlineKeyboardButton("👨‍💻 Dev 💻", callback_data='dev')
                 ],[
-                InlineKeyboardButton('📯 Updates', url='https://t.me/fsremote'),
-                InlineKeyboardButton('💁‍♂️ Soporte', url='https://t.me/feft05')
+                InlineKeyboardButton('♻️ Updates', url='https://t.me/fsremote'),
+                InlineKeyboardButton('🗨️ Soporte', url='https://t.me/feft05')
                 ],[
-                InlineKeyboardButton('🎛️ About', callback_data='about'),
-                InlineKeyboardButton('🛠️ Help', callback_data='help')
+                InlineKeyboardButton('📋 About', callback_data='about'),
+                InlineKeyboardButton('👮‍♂️ Help', callback_data='help')
             ]])
         )
     elif data == "help":
