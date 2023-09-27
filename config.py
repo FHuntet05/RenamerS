f498e7f1ef547a3f408a34458655b5b164cc83f0
FHuntet05 <129078356+FHuntet05@users.noreply.github.com>
2023-09-27 01:02:33 -0400

    config.py

--- config.py
+++ config.py
@@ -4,19 +4,19 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "27026389")
+    API_HASH  = os.environ.get("API_HASH", "158b014213c39d3b342a8792e495a5dc")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6139931468:AAEInK7S9neJ5Z_IlJx3bWE8s7JWDvamFfU") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","cluster0")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://Hunterbots:Hunterbots@cluster0.il15reh.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/5fc119c05326d2595965f.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1601545124').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001857528168") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
 
     # wes response configuration     
